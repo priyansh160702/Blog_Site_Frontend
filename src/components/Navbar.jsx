0ee0ba79fcde4250d12e028b0550c5cb91327bba
Priyansh-Antino <priyansh@antino.com>
2024-08-22 12:46:34 +0530

    Change visibility of Auth buttons based on path.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,31 @@
 import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 const Navbar = () => {
+  const location = useLocation();
+
+  const path = location.pathname;
+
+  const showAuth = path !== "/login" && path !== "/create-account";
+
   return (
-    <nav className="py-4">
+    <nav className="py-4 mb-7">
       <div className="container flex justify-between items-center">
         <Link to="/">
           <h1 className="font-semibold text-2xl">Nest-Level Blogs</h1>
         </Link>
-        <div>
-          <div className="flex space-x-2 items-center">
-            <Link to="login" className="auth-btn">
-              Log in
-            </Link>
-            <Link to="signup" className="auth-btn bg-black text-white ">
-              Create Account
-            </Link>
+        {showAuth && (
+          <div>
+            <div className="flex space-x-2 items-center">
+              <Link to="login" className="btn-white">
+                Log in
+              </Link>
+              <Link to="create-account" className="btn-black">
+                Create Account
+              </Link>
+            </div>
           </div>
-        </div>
+        )}
       </div>
     </nav>
   );
