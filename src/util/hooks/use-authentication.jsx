db6f8d30a74d69a08ac2e081656054ace630e066
Priyansh-Antino <priyansh@antino.com>
2024-08-29 15:53:35 +0530

    Add Auto logout functionality.

--- src/util/hooks/use-authentication.jsx
+++ src/util/hooks/use-authentication.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { jwtDecode } from "jwt-decode";
 
 const useAuthentication = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(
@@ -8,6 +9,25 @@ const useAuthentication = () => {
   const updateAuthState = () => {
     setIsAuthenticated(!!localStorage.getItem("token"));
   };
+
+  // Auto Logout
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      const decodedToken = jwtDecode(token);
+      const expirationTime = decodedToken.exp * 1000; // exp is in seconds, convert to ms
+      const currentTime = Date.now();
+      const remainingTime = expirationTime - currentTime;
+
+      if (remainingTime > 0) {
+        setTimeout(() => {
+          logout();
+        }, remainingTime);
+      } else {
+        logout();
+      }
+    }
+  }, [isAuthenticated]);
 
   const logout = () => {
     localStorage.removeItem("token");
