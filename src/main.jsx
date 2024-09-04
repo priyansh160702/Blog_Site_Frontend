c9a0bb816d3fd742e2931b7338cd6946732a5ecd
Priyansh-Antino <priyansh@antino.com>
2024-09-04 14:54:05 +0530

    Chakra UI setup.

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import { Provider } from "react-redux";
 import "./index.css";
+
+import { Provider } from "react-redux";
+import { ChakraProvider } from "@chakra-ui/react";
 
 import store from "./redux/store.jsx";
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </Provider>
 );
